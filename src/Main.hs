0e19f5f64ae847f7621e1aa9bbb1ab48f4c14c7c
Samuel Gélineau <gelisam+github@gmail.com>
2021-12-17 20:48:11 -0500

    display the png in a window

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 import Diagrams.Prelude (Diagram, circle)
 import Diagrams.Backend.Cairo.CmdLine (B, mainWith)
+import Graphics.UI.Gtk
 import System.Environment (withArgs)
 
 
@@ -14,4 +15,20 @@ main = do
   withArgs ["--output", "out.png"] $ do
     mainWith exampleDiagram
   
-  putStrLn "typechecks."
+  initGUI
+  let w = 640
+  let h = 480
+  dialog <- dialogNew
+  windowSetKeepAbove dialog True
+  windowSetDefaultSize dialog w h
+  dialogAddButton dialog stockClose ResponseClose
+  contain <- dialogGetUpper dialog
+  image <- imageNewFromFile "out.png"
+  af <- aspectFrameNew 0.5 0.5 (Just 1.6)
+  containerAdd af image
+  boxPackStartDefaults contain af
+
+  widgetShowAll dialog
+  dialogRun dialog
+  widgetDestroy dialog
+  flush
