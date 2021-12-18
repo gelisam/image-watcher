7ad886ef7986ae75da6e5ab863e238bef33bf86e
Samuel Gélineau <gelisam+github@gmail.com>
2021-12-17 20:32:50 -0500

    render example diagram to png

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,17 @@
-main :: IO ()
+import Diagrams.Prelude (Diagram, circle)
+import Diagrams.Backend.Cairo.CmdLine (B, mainWith)
+import System.Environment (withArgs)
+
+
+exampleDiagram
+  :: Diagram B
+exampleDiagram
+  = circle 10
+
+main
+  :: IO ()
 main = do
+  withArgs ["--output", "out.png"] $ do
+    mainWith exampleDiagram
+  
   putStrLn "typechecks."
